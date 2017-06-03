1796429a27d95b376f848ba25e2c4cad21e9e13e
moniqx4 <moniqx4@gmail.com>
2017-06-03 09:42:48 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
+
 'use strict'
 
+const express = require('express')
+const proxy = require('express-http-proxy')
+const bodyParser = require('body-parser')
 const _ = require('lodash')
-const fs = require('fs')
+const config = require('./config')
+const commands = require('./commands')
+const helpCommand = require('./commands/help')
 
-const commands = _.reduce(fs.readdirSync(__dirname), (a, file) => {
-  if (file !== 'index.js') a.push(require(`./${file}`))
+let bot = require('./bot')
 
-  return a
-}, [])
+let app = express()
 
-module.exports = commands
+if (config('PROXY_URI')) {
+  app.use(proxy(config('PROXY_URI'), {
+    forwardPath: (req, res) => { return require('url').parse(req.url).path }
+  }))
+}
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => { res.send('\n 👋 🌍 \n') })
+
+app.post('/commands/rulesbot', (req, res) => {
+  let payload = req.body
+
+  if (!payload || payload.token !== config('RULESBOT_COMMAND_TOKEN')) {
+    let err = '✋  Star—what? An invalid slash token was provided\n' +
+              '   Is your Slack slash token correctly configured?'
+    console.log(err)
+    res.status(401).end(err)
+    return
+  }
+
+  let cmd = _.reduce(commands, (a, cmd) => {
+    return payload.text.match(cmd.pattern) ? cmd : a
+  }, helpCommand)
+
+  cmd.handler(payload, res)
+})
+
+app.listen(config('PORT'), (err) => {
+  if (err) throw err
+
+  console.log(`\n🚀  Rulesbot LIVES on PORT ${config('PORT')} 🚀`)
+
+  if (config('SLACK_TOKEN')) {
+    console.log(`🤖  beep boop: @starbot is real-time\n`)
+    bot.listen({ token: config('SLACK_TOKEN') })
+  }
+})
